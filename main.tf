0e1873816104ab91531edd997281dfd75fa75ff8
Lavi Singodiya <lavi.singodiya@celebaltech.com>
2026-06-24 07:10:56 +0000

    Add gitignore

---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-lab-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "terraform-lab-public-subnet"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web.id
+  ]
+
+  tags = {
+    Name = "terraform-lab"
+  }
+}

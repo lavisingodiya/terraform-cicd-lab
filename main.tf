770af571029a8cce35f86500267df0e19e81a88d
Lavi Singodiya <lavi.singodiya@celebaltech.com>
2026-06-24 07:43:26 +0000

    add IGW

--- main.tf
+++ main.tf
@@ -55,3 +55,25 @@ resource "aws_instance" "web" {
     Name = "terraform-lab"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-lab-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
